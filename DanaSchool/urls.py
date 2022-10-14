42e2ba1cc5171e8631fab8bcad7fb7fda1c1df4e
AmeAya <ameameameaya@gmail.com>
2022-10-14 12:56:17 +0600

    remaned

--- DanaSchool/urls.py
+++ DanaSchool/urls.py
@@ -1,4 +1,4 @@
-"""dana_school_project URL Configuration
+"""DanaSchool URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/

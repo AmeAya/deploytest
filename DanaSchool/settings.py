a674bda33b7018fdb479ef508df2afe7c2a593fe
AmeAya <deobackstep@gmail.com>
2022-10-17 09:33:29 +0000

    deployed

--- DanaSchool/settings.py
+++ DanaSchool/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assert')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -126,4 +126,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = '/home/AmeAya/DanaSchool/static/images'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/AmeAya/DanaSchool/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")

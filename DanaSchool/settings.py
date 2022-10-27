dfea40146f15c1c130de2e8cb802db7b81f9cb92
AmeAya <ameameameaya@gmail.com>
2022-10-27 16:59:10 +0600

    qwe

--- DanaSchool/settings.py
+++ DanaSchool/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-(zv+&lj1lm9tue0z@ce8u3q#@%p#g#d+m&08-7c6gcqe*lk@!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['ameayaa.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['ameayaa.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
